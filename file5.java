86bd1cc88f58eae37c04c8125e1066ed8ec1af97
RajavinGayathri <ezhilgayathri@outlook.com>
2024-04-12 04:45:12 +0530

    Modifying the same line

--- file5.java
+++ file5.java
@@ -2,5 +2,6 @@ class file5
 {
     public static void main(String[] args) {
         System.out.println("Writing on file5.java");
+        System.out.println("Adding from local again to resolve conflict");
     }
 }
